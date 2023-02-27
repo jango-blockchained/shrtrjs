140fed9f469386de3bad09678bea41cdb743dbf9
RS Finance Labs <123484232+stepnalien@users.noreply.github.com>
2023-02-27 16:58:10 +0600

    latest v on op goerli

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -36,8 +36,8 @@ const graphURIEndpoints = {
   1: "https://api.thegraph.com/subgraphs/name/ensdomains/ens",
   3: "https://api.thegraph.com/subgraphs/name/ensdomains/ensropsten",
   4: "https://api.thegraph.com/subgraphs/name/ensdomains/ensrinkeby",
-  5: "https://api.thegraph.com/subgraphs/name/stepnalien/minegoerli",
-  420: "https://api.thegraph.com/subgraphs/name/opnames/onsgoerli"
+  5: "https://api.thegraph.com/subgraphs/name/grmkris/op-ens",
+  420: "https://api.thegraph.com/subgraphs/name/opnames/opgoerli"
 };
 class ONS {
   constructor(options) {
--- dist/esm/index.mjs
+++ dist/esm/index.mjs
@@ -8,8 +8,8 @@ var graphURIEndpoints = {
   1: "https://api.thegraph.com/subgraphs/name/ensdomains/ens",
   3: "https://api.thegraph.com/subgraphs/name/ensdomains/ensropsten",
   4: "https://api.thegraph.com/subgraphs/name/ensdomains/ensrinkeby",
-  5: "https://api.thegraph.com/subgraphs/name/stepnalien/minegoerli",
-  420: "https://api.thegraph.com/subgraphs/name/opnames/onsgoerli"
+  5: "https://api.thegraph.com/subgraphs/name/grmkris/op-ens",
+  420: "https://api.thegraph.com/subgraphs/name/opnames/opgoerli"
 };
 var ONS = class {
   options;
